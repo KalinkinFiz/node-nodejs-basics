85915378dda7a7461d50cbe5919f909e3dca82e6
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-11-28 22:54:36 +0300

    feat: file creation done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+
+const CONTENT = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile("./src/fs/files/fresh.txt", CONTENT, { flag: "wx" });
+    console.log(
+      "The file has been successfully created, the information has been entered"
+    );
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      throw new Error("FS operation failed");
+    } else {
+      throw new Error(err.stack);
+    }
+  }
 };
 
 await create();
