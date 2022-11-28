7aa46206ad92c0d283205f08a39365e0ce98461d
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-11-28 23:20:13 +0300

    feat: copying files done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await fs.cp("./src/fs/files", "./src/fs/files_copy", {
+      force: false,
+      errorOnExist: true,
+      recursive: true,
+    });
+    console.log("All files moved to files_copy folder");
+  } catch (err) {
+    if (err.code === "ERR_FS_CP_EEXIST") {
+      throw new Error("FS operation failed");
+    } else {
+      throw new Error(err.stack);
+    }
+  }
 };
 
 copy();
