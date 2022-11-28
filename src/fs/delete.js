e29131dc9c058ba14460816d6d8428f02d2bd299
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-11-29 02:06:59 +0300

    feat: delete files done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import fs from "fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir("./src/fs/files");
+    if (!files.includes("fileToRemove.txt"))
+      throw new Error("FS operation failed");
+    await fs.rm("./src/fs/files/fileToRemove.txt", {
+      force: true,
+    });
+    console.log("Files deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 await remove();
