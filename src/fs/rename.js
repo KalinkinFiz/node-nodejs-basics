8c763a58731681c65399ec75437964ae18b67f5c
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-11-29 01:38:11 +0300

    feat: rename files done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from "fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir("./src/fs/files");
+    if (
+      !files.includes("wrongFilename.txt") ||
+      files.includes("properFilename.md")
+    ) {
+      throw new Error("FS operation failed");
+    } else {
+      await fs.rename(
+        "./src/fs/files/wrongFilename.txt",
+        "./src/fs/files/properFilename.md"
+      );
+      console.log("Files rename");
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 await rename();
