8528712ac3bc03d7b677b8dca6d1d2d70b2e73bb
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-11-29 19:11:36 +0300

    feat: args done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const props = [];
+  const values = [];
+  const out = [];
+
+  for (let item of args) {
+    if (item.includes("--")) {
+      props.push(item.slice(2));
+    } else {
+      values.push(item);
+    }
+  }
+
+  for (let i = 0; i < props.length; i++) {
+    output.push(`${props[i]} is ${values[i]}`);
+  }
+  console.log(out.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -2,7 +2,7 @@ const parseEnv = () => {
   const env = Object.entries(process.env);
   console.log(
     env
-      .filter((arr) => arr[0].startsWith("RSS_"))
+      .filter((arr) => arr[0].includes("RSS_"))
       .map((arr) => arr.join("="))
       .join("; ")
   );
