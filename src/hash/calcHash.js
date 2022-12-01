f7bc29ae4f50f05decc5c66549df9be0684f0fbe
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-02 00:51:32 +0300

    feat: hash done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createReadStream } from "fs";
+import { createHash } from "crypto";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const input = createReadStream(
+    path.join(__dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
