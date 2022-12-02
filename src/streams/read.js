eb576a2bde3ee7f5b380fef6ac84b57e8f399ed8
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-02 10:31:48 +0300

    feat: readable stream done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,31 @@
+import { createReadStream } from "fs";
+import { stdout } from "process";
+import { fileURLToPath } from "url";
+import { Readable } from "stream";
+import path from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+class MyReadable extends Readable {
+  constructor(opr) {
+    super(opr);
+  }
+
+  _read() {
+    const input = createReadStream(
+      path.join(__dirname, "files", "fileToRead.txt")
+    );
+
+    input.on("data", (chunk) => {
+      stdout._write(chunk);
+    });
+  }
+}
+
 const read = async () => {
-    // Write your code here 
+  const readableStream = new MyReadable();
+
+  readableStream.read();
 };
 
 await read();
