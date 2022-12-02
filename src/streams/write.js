604a353c499f5323c3fbf6578f4f1af37a0b2549
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-02 12:16:12 +0300

    feat: writable stream done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { createWriteStream } from "fs";
+import { stdin } from "process";
+import { fileURLToPath } from "url";
+import { Writable } from "stream";
+import path from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+class MyWritable extends Writable {
+  constructor(opr) {
+    super(opr);
+  }
+
+  input = createWriteStream(path.join(__dirname, "files", "fileToWrite.txt"), {
+    encoding: "utf-8",
+  });
+
+  _write(chunk, _encoding, callback) {
+    this.input.write(chunk);
+    callback;
+  }
+}
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = new MyWritable();
+  stdin.pipe(writableStream);
 };
 
 await write();
