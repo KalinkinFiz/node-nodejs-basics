96467419787fd2b5a781da53af76cce06a60663c
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-03 11:18:01 +0300

    feat: transform stream done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,37 @@
+import { Transform, pipeline } from "stream";
+import { promisify } from "util";
+
+const pipelineAsync = promisify(pipeline);
+
+class MyTransform extends Transform {
+  constructor(opr) {
+    super(opr);
+  }
+
+  _transform(chunk, _, done) {
+    const transformChunk = chunk
+      .toString("utf-8")
+      .trim()
+      .split("")
+      .reverse()
+      .join("");
+
+    this.push(transformChunk + "\n");
+    done();
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new MyTransform();
+  const ReadableStream = process.stdin;
+  const WriteableStream = process.stdout;
+
+  try {
+    await pipelineAsync(ReadableStream, transformStream, WriteableStream);
+  } catch (e) {
+    process.stderr.write(e.message);
+    process.exit(1);
+  }
 };
 
 await transform();
