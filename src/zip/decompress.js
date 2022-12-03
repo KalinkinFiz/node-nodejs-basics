1200438135ab5cd179ac7d0b51d7554b1d74691d
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-03 14:12:33 +0300

    feat: decompress done

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import path from "path";
+import url from "url";
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const pipelineAsync = promisify(pipeline);
+
 const decompress = async () => {
-    // Write your code here 
+  const input = createReadStream(path.join(__dirname, "files", "archive.gz"));
+  const zip = createGunzip();
+  const output = createWriteStream(
+    path.join(__dirname, "files", "fileToCompress1.txt")
+  );
+
+  try {
+    await pipelineAsync(input, zip, output);
+  } catch (e) {
+    process.stderr.write(e.message);
+    process.exit(1);
+  }
 };
 
 await decompress();
