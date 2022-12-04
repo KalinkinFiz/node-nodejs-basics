967116acb3c91eaa050fdaed7a1489f0d5da314e
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-05 00:37:01 +0300

    feat: worker done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import { Worker } from "worker_threads";
+import { fileURLToPath } from "url";
+import { cpus } from "os";
+import path from "path";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const performCalculations = async () => {
-    // Write your code here
+  const promises = [];
+
+  for (let i = 0; i < cpus().length; i++) {
+    promises.push(
+      new Promise((resolve, _reject) => {
+        const worker = new Worker(path.join(__dirname, "worker.js"), {
+          workerData: 10 + i,
+        });
+
+        worker.on("message", (data) => resolve({ status: "resolved", data }));
+        worker.on("error", () => resolve({ status: "error", data: null }));
+      })
+    );
+  }
+
+  Promise.all(promises).then((result) => console.log(result));
 };
 
 await performCalculations();
