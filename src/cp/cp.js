9fde29f0723d2a3c7a8a09f5b8a87008a99f8f7e
KalinkinFiz <kalinkin.fiz@gmail.com>
2022-12-04 17:06:23 +0300

    feat: cp done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,14 @@
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+import path from "path";
+
+const args = process.argv.slice(2);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  fork(path.join(__dirname, "files", "script.js"), args, {
+    stdio: [process.stdin, process.stdout, "ipc"],
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess(args);
